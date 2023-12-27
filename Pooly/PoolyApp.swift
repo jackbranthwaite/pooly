3931854c636046adb8231f3181885e87e5bdac83
Jack <jack@clicksuite.co.nz>
2023-12-28 11:29:03 +1300

    Initial Commit

---
+++ Pooly/PoolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolyApp.swift
+//  Pooly
+//
+//  Created by Jack on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PoolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
