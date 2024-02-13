9440f72947e136c31de72987c5db84f8fa743581
Jack Branthwaite <jack@formfunfunction.com>
2024-02-13 16:51:42 +1300

    Progress on sheet

--- Pooly/Views/PlayPage/PlayCard/PlayCardView.swift
+++ Pooly/Views/PlayPage/PlayCard/PlayCardView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct PlayCardView: View {
+    
+    @State private var showSheet: Bool = false
+    
     var body: some View {
         HStack {
             Circle()
@@ -33,6 +36,30 @@ struct PlayCardView: View {
         .overlay(RoundedRectangle(cornerRadius: 12).stroke( (Color(red: 229/255, green: 229/255, blue: 229/255)), lineWidth: 1))
         .background(Color(red: 241/255, green: 245/255, blue: 251/255).clipShape(RoundedRectangle(cornerRadius:12)))
         .padding(.top, 30)
+        .onTapGesture {
+            showSheet.toggle()
+        }
+        .sheetPresentation(isPresented: $showSheet, sheetView: {
+            VStack(alignment: .leading){
+                ScrollView() {
+                    AccountTileView()
+                }
+                .padding(.top, 20)
+                HStack {
+                    PrimaryButtonView(text: "Play Now" ) {
+                        print("Play time")
+                    }
+                    .padding()
+                }
+                .background(Color.white)
+                .overlay(Rectangle().frame(width: nil, height: 0.25, alignment: .top).foregroundColor(Color.gray), alignment: .top)
+            }
+                .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(Color.white)
+                
+        }, onDismiss: {
+           
+        })
     }
     
    
